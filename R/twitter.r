8ab65f2a06254675d0c8bd5d6e581d4a7e72a039
anak10thn <anak10thn@gmail.com>
2015-04-28 21:51:54 +0700

    ADD: fucking world

--- R/twitter.r
+++ R/twitter.r
@@ -1,5 +1,5 @@
 library(twitteR)
-setup_twitter_oauth('7nMVOHZ1KjcBDWJV2PJgtw','dhmIVLaoY4jL3jLGEU3uPH9TkE7Xsnv7JC0jyeY54')
+setup_twitter_oauth('','')
 tl <- userTimeline('milisdad', n=1000, includeRts = TRUE)
 tl.df <- twListToDF(tl)
 tl.df$text <- iconv(tl.df$text, to='utf-8-mac')
